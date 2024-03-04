1b2b1ea990098165c13815a696d26b7bd68a2ed4
Sagar Jha <sagarjha0511@gmail.com>
2024-03-04 12:42:14 +0530

    ok

---
+++ basics/variables.js
@@ -0,0 +1,8 @@
+const accountId= 555;
+let emailId = "oggy@gmail.com";
+emailId = "oggy1@gmail.com";
+var Password = 12345;
+var Password = 12541;
+Password = 54645;
+console.table([accountId,emailId,Password]);
+
