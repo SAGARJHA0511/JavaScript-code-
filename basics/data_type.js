58802f4e6cd9abb9dcafdace60e16c02e46949fc
Sagar Jha <sagarjha0511@gmail.com>
2024-03-04 15:13:29 +0530

    DT

---
+++ basics/data_type.js
@@ -0,0 +1,14 @@
+// To store data in variables 
+
+//  There are 2 types of data types 
+//  - Primitive data type           and                   Non-primitive data type 
+//    "Hello"       --- String                            ["hello"]      ---- Array
+//     25           --- Number                            {       }       ---- Object 
+//    Ture or Fales  --- Boolen                           represent reguler 
+//    Var x;         --- Undefined                        Expression         ----- RegExp
+//    Null           --- Null(No Value at all)
+//    12345678909292 ---- Bigint
+//    Symbol         ---  $(Uniqness)
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); // Object 
--- basics/variables.js
+++ basics/variables.js
@@ -3,6 +3,8 @@ let emailId = "oggy@gmail.com";
 emailId = "oggy1@gmail.com";
 var Password = 12345;
 var Password = 12541;
+let state;
 Password = 54645;
-console.table([accountId,emailId,Password]);
+
+console.table([accountId,emailId,Password,state]);
 
