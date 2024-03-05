a20292fa02f0fa096b9aaa092562621e35160c64
Sagar Jha <sagarjha0511@gmail.com>
2024-03-06 00:55:37 +0530

    object

--- basics/data_type.js
+++ basics/data_type.js
@@ -10,5 +10,19 @@
 //    12345678909292 ---- Bigint
 //    Symbol         ---  $(Uniqness)
 
-console.log(typeof undefined); //undefined
-console.log(typeof null); // Object 
+// console.log(typeof undefined); //undefined
+// console.log(typeof null); // Object 
+
+
+// const size = [28,32,36]
+// console.log(typeof size);   // when you use typeof on an array, it simply tells you that it's an object
+
+// const age = [13, 15, 34, 43];   // If you want to specifically check if a variable is an array, you can use the Array.isArray() method
+// console.log(Array.isArray(age));  // output will true 
+   
+
+let growth;
+growth = "string "
+growth = 100 
+
+console.log(growth);
