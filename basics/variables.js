6817cbb8bf1b4bc0c671755de9c1196a10f42ecb
Sagar Jha <sagarjha0511@gmail.com>
2024-03-07 23:51:45 +0530

    memory allocation

--- basics/variables.js
+++ basics/variables.js
@@ -6,5 +6,15 @@ var Password = 12541;
 let state;
 Password = 54645;
 
-console.table([accountId,emailId,Password,state]);
+// console.table([accountId,emailId,Password,state]);
 
+// let dessert = "cake";
+// let dessert = "ice-crem"
+// console.log(dessert)
+
+   function changeColor() {
+    let x = ("p1")
+    x.style.color = "green";
+    };
+    changeColor();
+    console.log(changeColor);
