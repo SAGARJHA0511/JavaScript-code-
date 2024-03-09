da1e1ee00064f0497976407f21ca0a077a7fa62e
Sagar Jha <sagarjha0511@gmail.com>
2024-03-09 23:54:38 +0530

    math

---
+++ basics/maths.js
@@ -0,0 +1,21 @@
+// math methods to preform mathematical operation
+
+// Absolute method 
+console.log(Math.abs(-5));// gives absolute value // Output => 5 
+
+// Round method 
+console.log(Math.round(3.6)); // round a number to the nearest integer // output => 4
+
+//ceil method 
+console.log(Math.ceil(4.1)) // rounds up to the nearest number // output => 5
+
+// floor method 
+console.log(Math.floor(4.9)); // rounds a number down to the nearest integer // Output => 4
+
+// square method 
+console.log(Math.sqrt(9)); // returns the square root of a number  // Output => 3
+
+// pow method 
+console.log(Math.pow(2,3)); // returns the base to the exponent power, that is base^exponent eg- 2^3 2*2*2 = 8 // Output => 8
+
+
