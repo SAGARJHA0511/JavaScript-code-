97d4183a8284019d15b5ebf8bb0ab36c3257ffbd
Sagar Jha <sagarjha0511@gmail.com>
2024-03-10 03:17:29 +0530

    math

--- basics/maths.js
+++ basics/maths.js
@@ -19,3 +19,11 @@ console.log(Math.sqrt(9)); // returns the square root of a number  // Output => 
 console.log(Math.pow(2,3)); // returns the base to the exponent power, that is base^exponent eg- 2^3 2*2*2 = 8 // Output => 8
 
 
+// random method 
+let min = 1;
+max = 6;
+console.log(Math.floor(Math.random()*(max-min + 1))+ min); // to get value between 1 to 6 
+//
+console.log(Math.random()); // gives value between 0 and 1 ===> like this 0.1234 or 0.65432
+console.log(Math.random()*10)  // This expression will generate random floating-point numbers between 0 (inclusive) and 10 (exclusive), effectively
+console.log(Math.random()*10 + 1) //When we add +1 to the expression, This means the range becomes [1, 11) instead of [0, 10)  ==> To avoid 0 
