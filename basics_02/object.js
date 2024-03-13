358796ce7843f4e829bfc09b52089c54b0ccf357
Sagar Jha <sagarjha0511@gmail.com>
2024-03-14 00:41:25 +0530

    ways

---
+++ basics_02/object.js
@@ -0,0 +1,39 @@
+// There are 3 ways to create object 
+// By object literal
+// By creating instance of Object directly (using new keyword)
+// By using an object constructor (using new keyword) 
+
+// Object literal 
+
+let person = {
+    name : 'Nagdiv',
+    age : 34,
+    salary : 45000,
+    location : ["Bangolore", "Noida", "USA"],
+    greet : function(){
+        console.log("hello ! welcome to my profile");
+    }
+    
+
+}
+console.log(typeof person);
+//accessing the object value 
+console.log(person.name);;
+console.log(person.age);
+console.log(person.location[0]);
+console.log(person.greet());
+
+// constructor 
+
+function Employee(){
+    this.name = "gama";
+    this.salary = 45000;
+
+}
+let emp = new Employee();
+console.log(typeof emp);
+console.log(emp);
+//accessing the object value 
+console.log(emp.name,emp.salary);
+console.log(emp.name);
+console.log(emp.salary);
