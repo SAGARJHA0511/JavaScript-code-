b0fe8c7b4d2a8bcbfb516dcdc874d172ee26a8ec
Sagar Jha <sagarjha0511@gmail.com>
2024-03-14 01:23:06 +0530

    Keys & values

---
+++ basics_02/obj_pro_s.js
@@ -0,0 +1,25 @@
+// object is non-primitive data-type that allows to store multiple collections of data.
+/* 
+=> group of key values pairs as one entity.
+=> group of properties (property name & property value pairs) as one entity.
+=> group of data and functionality as one entity.
+=> duplicates keys/property name are not allowed.
+*/ 
+
+// The syntax to declare an object 
+
+const ObjectName = {
+    key1 : "value1",
+    key2 : "value2",
+
+};
+console.log(ObjectName);
+console.log(ObjectName.key1)
+
+const Employee = {
+    name : "deep patil",
+    Salary : 33000    
+//  [key]    [value]
+
+};
+console.log(Employee);
